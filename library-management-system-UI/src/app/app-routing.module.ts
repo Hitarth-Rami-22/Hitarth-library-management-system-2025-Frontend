30e69a1bd8847c517911f5bf4e97ab6ace7fecc7
Hitarth-Rami-22 <hitarth.rami@bacancy.com>
2025-06-10 14:11:08 +0530

    basic-structure

--- library-management-system-UI/src/app/app-routing.module.ts
+++ library-management-system-UI/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PageHeaderComponent } from './shared/components/page-header/page-header.component';
+import { SharedModule } from './shared/shared.module';
+import { PageNotFoundComponent } from './shared/components/page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+
+  {path: "**", component: PageNotFoundComponent},
+
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ library-management-system-UI/src/app/shared/components/page-side-nav/page-side-nav.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+export interface NavigationItem {
+  value: string;
+  link: string;
+}
+@Component({
+  selector: 'app-page-side-nav',
+  templateUrl: './page-side-nav.component.html',
+  styleUrls: ['./page-side-nav.component.scss']
+})
+export class PageSideNavComponent {
+  panelName: string = 'Student Panel';
+  navItems: NavigationItem[] = [];
+
+  constructor(){
+    this.navItems=[
+      {value: 'View Books', link: '/view-books'},
+      {value: 'My Order', link: '/my-order'},
+    ]
+  }
+}
