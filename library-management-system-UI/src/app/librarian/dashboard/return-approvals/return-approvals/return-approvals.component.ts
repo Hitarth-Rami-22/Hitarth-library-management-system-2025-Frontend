81faf6a383b78d2c024b682faf0f9b72a91e2882
Hitarth-Rami-22 <hitarth.rami@bacancy.com>
2025-06-18 18:26:11 +0530

    changed-UI

--- library-management-system-UI/src/app/librarian/dashboard/return-approvals/return-approvals/return-approvals.component.ts
+++ library-management-system-UI/src/app/librarian/dashboard/return-approvals/return-approvals/return-approvals.component.ts
@@ -1,14 +1,15 @@
+
 import { Component, OnInit } from '@angular/core';
 import { BorrowServiceService } from 'src/app/student/borrow-service/borrow-service.service';
+
 @Component({
   selector: 'app-return-approvals',
   templateUrl: './return-approvals.component.html',
   styleUrls: ['./return-approvals.component.scss']
 })
 export class ReturnApprovalsComponent implements OnInit {
-   returnRequests: any[] = [];
-   approvedBooks: any[] = [];
-   studentId: number = 0;
+  returnRequests: any[] = [];
+  isLoading: boolean = true;
 
   constructor(private borrowService: BorrowServiceService) {}
 
@@ -16,24 +17,27 @@ export class ReturnApprovalsComponent implements OnInit {
     this.loadReturnRequests();
   }
 
- loadReturnRequests() {
-  this.borrowService.getAllRequests().subscribe({
-    next: (res: any[]) => {
-      this.returnRequests = res.filter(r => r.status === 4); // 4 = Return Requested
-    },
-    error: () => alert('Failed to load return requests')
-  });
-}
-
-
+  loadReturnRequests() {
+    this.isLoading = true;
+    this.borrowService.getAllRequests().subscribe({
+      next: (res: any[]) => {
+        this.returnRequests = res.filter(r => r.status === 4); // 4 = Return Requested
+        this.isLoading = false;
+      },
+      error: () => {
+        alert('Failed to load return requests');
+        this.isLoading = false;
+      }
+    });
+  }
 
   approveReturn(borrowId: number) {
     this.borrowService.updateStatus({ requestId: borrowId, newStatus: 3 }).subscribe({
       next: () => {
-        alert('✅ Book marked as Returned');
+        alert('Book successfully marked as returned!');
         this.loadReturnRequests();
       },
-      error: () => alert('❌ Failed to approve return')
+      error: () => alert('Failed to approve return')
     });
   }
 }
